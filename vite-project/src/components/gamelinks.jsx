395e980b4872dd449c83f33672da97c09c3226db
rafael vincent <d749107@gmail.com>
2025-08-01 09:20:00 +0800

    w

---
+++ vite-project/src/components/gamelinks.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const gamelinks = () => {
+  return (
+    <div>
+      <h3>Useful game links</h3>
+      <li>
+        <a
+          href="https://central.github.com/deployments/desktop/desktop/latest/win32"
+          download
+        >
+          Download dota skins here
+        </a>
+      </li>
+    </div>
+  );
+};
+
+export default gamelinks;
