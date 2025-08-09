0ec6184da81ae14ae12f8e59324c2abc842ff3f4
rafael vincent <d749107@gmail.com>
2025-08-09 19:58:18 +0800

    added chakraa ui

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css'
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
