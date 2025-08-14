d5ccf17cc782e40ee3e7cbc3407afa2e01b5eeb0
rafael vincent <d749107@gmail.com>
2025-08-14 09:50:41 +0800

    hey

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} bitch`);
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
 });
