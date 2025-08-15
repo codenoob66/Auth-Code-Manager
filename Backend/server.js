09f580fe0fadc411e34b1a11c6828251079460e8
rafael vincent <d749107@gmail.com>
2025-08-15 08:46:33 +0800

    hey

--- Backend/server.js
+++ Backend/server.js
@@ -36,6 +36,10 @@ connectDB();
 // Routes
 app.use("/api/codes", codeRoutes);
 
+app.get("/", (req, res) => {
+  res.send("hey  you've reached the backend server, you should not be here!");
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
